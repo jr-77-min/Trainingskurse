0cc9139476366351189ab50a45fa7fca47bd9dea
gitRodd <rodrigue.lawson@edu.siemens-energy.com>
2021-04-27 15:10:46 +0200

    Aktuell

--- Trainingsplan2/src/application/ProfilÜbersicht.java
+++ Trainingsplan2/src/application/ProfilÜbersicht.java
@@ -1,15 +1,15 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
+public class Profil‹bersicht extends Application {
 
-public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Start.fxml"));
 			Scene scene = new Scene(root,400,400);
@@ -20,8 +20,9 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
